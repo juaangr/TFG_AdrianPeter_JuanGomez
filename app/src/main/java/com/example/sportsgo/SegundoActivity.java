fa45f47fdd81d4c64f875d9639b29128060836a9
Adrián Alexandru Peter <adrianpeter277@gmail.com>
2026-03-12 17:01:18 +0100

    Update2 SegundoActivity.java

--- app/src/main/java/com/example/sportsgo/SegundoActivity.java
+++ app/src/main/java/com/example/sportsgo/SegundoActivity.java
@@ -10,7 +10,6 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.sportsgo.R;
 
