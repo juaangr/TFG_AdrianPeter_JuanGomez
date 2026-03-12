5300c25b5dd114a1b4229acdf39f48370e659ed8
Adrián Alexandru Peter <adrianpeter277@gmail.com>
2026-03-12 16:58:05 +0100

    Update SegundoActivity.java

--- app/src/main/java/com/example/sportsgo/SegundoActivity.java
+++ app/src/main/java/com/example/sportsgo/SegundoActivity.java
@@ -12,7 +12,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.sportsgo.R;
 
 public class SegundoActivity extends AppCompatActivity {
