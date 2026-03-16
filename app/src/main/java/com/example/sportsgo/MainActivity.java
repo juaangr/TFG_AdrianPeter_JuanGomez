0602e603ad44b3d8b2831ae4c20ac7895bb3aa0e
juang <rodriguezgomezj195@gmail.com>
2026-03-16 18:25:51 +0100

    primeros pasos para logins y viceversa

--- app/src/main/java/com/example/sportsgo/MainActivity.java
+++ app/src/main/java/com/example/sportsgo/MainActivity.java
@@ -17,60 +17,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.sportsgo.R;
 
+/*
+* activity sin vista para manejar y redirigir a otras vistas
+* */
 public class  MainActivity extends AppCompatActivity {
-    private EditText etNombre, etEdad;
-    private Button btn;
-    @SuppressLint("MissingInflatedId")
+
+    // recuperamos preferencias y si es la primera vez que se abre la app (que es lo mismo que no estar logeado)
+    SharedPreferences prefs = getSharedPreferences("PrefeSportsGO", MODE_PRIVATE);
+    boolean estaLogueado = prefs.getBoolean("logueado", false);
+    boolean noLogeado = prefs.getBoolean("noLogueado", true);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        etNombre  = findViewById(R.id.etNombre);
-        etEdad = findViewById(R.id.etEdad);
-        btn = findViewById(R.id.btnIniciar);
 
-        //Recuperar preferencias al iniciar la app
-        SharedPreferences prefs = getSharedPreferences("PrefeSportsGO", Context.MODE_PRIVATE);
-        String nombreGuardado = prefs.getString("user_nombre","");
-        etNombre.setText(nombreGuardado);
+        // pagina para elegir logearse como admin, user (pupilo) o trainer
+        if(noLogeado) startActivity(new Intent(this, ElegirLoginActivity.class));
+        // lo que podría ser una especie de tl rollo tw o instagram... esto no se puede descomentar a menos que tengamos la pantalla principal hecha
+        // else if (estaLogueado) startActivity(new Intent(this, ));
 
-        btn.setOnClickListener(v -> {
-            String nombre = etNombre.getText().toString().trim();
-            String edadStr = etEdad.getText().toString().trim();
-
-            if(nombre.isEmpty() || edadStr.isEmpty()){
-                Toast.makeText(this, "Porfavor rellene todos los campos", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            int edad  = Integer.parseInt(edadStr);
-
-            if(edad >=18){
-                DialogoAlerta.mostrar(this, new DialogoAlerta.OnResultadoDialogo() {
-                    @Override
-                    public void alAceptar() {
-                        SharedPreferences.Editor editor = prefs.edit();
-                        editor.putString("user_nombre", nombre);
-                        editor.putInt("user_edad", edad);
-                        editor.apply();
-
-                        Intent intent = new Intent(MainActivity.this, SegundoActivity.class);
-                        intent.putExtra("key_nombre", nombre);
-                        intent.putExtra("key_edad", edad);
-                        startActivity(intent);
-                        Toast.makeText(MainActivity.this, "Bienvenido a nuestra pagina de deporte sportsGO", Toast.LENGTH_SHORT).show();
-
-                    }
-                });
-            }else {
-                Toast.makeText(this, "Debes de ser mayor de edad para acceder", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        finish();
     }
 }
