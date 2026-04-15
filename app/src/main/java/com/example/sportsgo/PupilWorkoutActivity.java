9bb6b3d6c427fad7e9ab5869ee2ad7de77f01978
Adrianpeter@Practicas <adrianpeter277@gmail.com>
2026-04-15 17:30:12 +0200

    Actualizacion de los datos del tercer_activity.xml -> activity_pupil_workout

--- app/src/main/java/com/example/sportsgo/PupilWorkoutActivity.java
+++ app/src/main/java/com/example/sportsgo/PupilWorkoutActivity.java
@@ -26,7 +26,7 @@ public class PupilWorkoutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.tercer_activity);
+    setContentView(R.layout.activity_pupil_workout);
 
     //Inicializamos realm
     realm = Realm.getDefaultInstance();
