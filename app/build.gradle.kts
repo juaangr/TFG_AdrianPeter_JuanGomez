0c2692f9816a1c81751785604cfd9186d1f2febf
Adrianpeter@Practicas <adrianpeter277@gmail.com>
2026-04-16 14:55:12 +0200

    Probando funcionamiento #5 (eliminando comentarios)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,8 @@
 import java.util.Properties
 
-// Plugin o mejor dicho interruptor para activar las funciones de Realm/MongoDB
 plugins {
     alias(libs.plugins.android.application)
-    id("realm-android")                    // Activa Realm
+    id("realm-android")
     id("com.google.gms.google-services")
 }
 
@@ -20,7 +19,6 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
-        // Cargar la API KEY desde local.properties
         val properties = Properties()
         val localPropertiesFile = project.rootProject.file("local.properties")
         if (localPropertiesFile.exists()) {
@@ -57,36 +55,23 @@ android {
     }
 }
 
-// Activamos la sincronización con MongoDB Atlas (Device Sync)
 realm {
     isSyncEnabled = true
 }
 
 dependencies {
-    // LIBRERÍA DE ADAPTADORES: Necesaria para el EjercicioAdapter y RealmBaseAdapter
-    //implementation("io.realm:android-adapters:3.1.0")
-    // LIBRERIA PARA LA NUBE: FIREBASE
-    //implementation("com.google.firebase:firebase-database:21.0.0")
-
-    // Dependencias base del proyecto (Material Design 3 y AndroidX)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
 
-    //Import del Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:34.11.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-auth")
-
-    // Google Gemini AI
     implementation("com.google.ai.client.generativeai:generativeai:0.9.0")
-
-    // Base de datos en tiempo real para el chat
     implementation("com.google.firebase:firebase-database")
 
-    // Pruebas unitarias e instrumentales
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
