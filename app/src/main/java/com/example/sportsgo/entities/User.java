161e5b6896fe3c7341b61aede29996ee96c7a389
Adrianpeter@Practicas <adrianpeter277@gmail.com>
2026-04-28 18:13:19 +0200

    Añade atributos calorías y permisoCompleto a la clase User

--- app/src/main/java/com/example/sportsgo/entities/User.java
+++ app/src/main/java/com/example/sportsgo/entities/User.java
@@ -5,6 +5,8 @@ public class User {
     private String uid;
     private String nombre;
     private String email;
+    private int calorias;
+    private boolean permisoCompleto;
 
     // Constructor vacío obligatorio para Firestore
     public User() {}
@@ -13,6 +15,8 @@ public class User {
         this.uid = uid;
         this.nombre = nombre;
         this.email = email;
+        this.calorias = 0;
+        this.permisoCompleto = false;
     }
 
     public String getUid() { return uid; }
@@ -21,4 +25,10 @@ public class User {
     public void setNombre(String nombre) { this.nombre = nombre; }
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+
+    public int getCalorias() { return calorias; }
+    public void setCalorias(int calorias) { this.calorias = calorias; }
+
+    public boolean isPermisoCompleto() { return permisoCompleto; }
+    public void setPermisoCompleto(boolean permisoCompleto) { this.permisoCompleto = permisoCompleto; }
 }
